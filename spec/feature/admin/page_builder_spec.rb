70bac58dc69a7a417a799c09ef9b99680d1bb626
César Carruitero <ccarruitero@protonmail.com>
2022-01-04 13:04:25 -0500

    add some feature specs

---
+++ spec/feature/admin/page_builder_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'Pages Builder', type: :feature, js: true do
+  context 'as admin user' do
+    stub_authorization!
+
+    before do
+      visit spree.admin_path
+    end
+
+    it 'should have a link to page builder' do
+      expect(page).to have_link('Pages Builder')
+    end
+
+    it 'allow create new page' do
+      expect {
+        click_on 'Pages Builder'
+        click_on 'New Page'
+        fill_in 'page_name', with: 'Test'
+        fill_in 'page_path', with: '/test'
+        click_on 'Create Page'
+      }.to change { Cms::Page.count }.by(1)
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'rake'
+require 'pry'
 
 # Configure Rails Environment
 ENV['RAILS_ENV'] = 'test'
