f04ddda6d06a3d051598ff6e411bba3744816905
Kevin Perez <kevin.perez@magmalabs.io>
2024-01-10 18:11:15 -0600

    Refactor classname into model_class
    This way the actual class in use can be overridden in a subclass.

--- app/controllers/spree/solidus_cms/pages_controller.rb
+++ app/controllers/spree/solidus_cms/pages_controller.rb
@@ -5,13 +5,19 @@ module Spree::SolidusCms
     layout SolidusCms.config.layout if SolidusCms.config.layout.present?
 
     def show
-      @page = SolidusCms::Page.active.find_by(path: request.path_info)
+      @page = model_class.active.find_by(path: request.path_info)
     end
 
     def preview
-      authorize! :manage, SolidusCms::Page
-      @page = SolidusCms::Page.find_by(path: params[:path])
+      authorize! :manage, model_class
+      @page = model_class.find_by(path: params[:path])
       render :show
+    end
+
+    private
+
+    def model_class
+      SolidusCms::Page
     end
   end
 end
